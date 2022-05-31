e4a0e176c0d4745a0a2444dc3f77f3dc8afa9dda
VirtualZer0 <virtualos@list.ru>
2022-06-01 01:29:08 +0300

    Add editor link to main screen

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    <router-link to="/editor">Editor</router-link>
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
